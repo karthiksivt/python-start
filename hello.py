bd003d3f3c66b4f26d66fc5387e0284f91d42f16
karthik <karthiksivt@gmail.com>
2023-09-06 19:40:58 +0530

    python test

--- hello.py
+++ hello.py
@@ -1,1 +1,46 @@
-print("Hello")
+# print("Hello")
+# print("hello","world","enter","python", sep='-', end='  ')
+
+
+# '''only end will work for next line code snippet'''
+
+
+# print("hello","world","enter","python", sep='-')
+
+# print("hello","world","enter","python", sep='-')
+
+#comments here
+
+#Dynamic typing
+
+# No need to to declare the variable like other language in C ex: int a =5 , Python is Dynamically typed language
+
+# Dyanamic typed: ex: name='kode' print(name).... but the variable as to be declared in correct format.
+
+#name=1234kart
+#print(name)
+
+#The above line invalid decimal literal error
+
+
+#Dynamic binding
+# for example in the same program i can assign a value multiple times like name='karthik, name ='kode' but
+# in other language it is not possible. its called static binding
+
+# diff types to print or assign values to the variable. 
+
+# a=b=c=6
+# a=5;b=6;c=7
+# a,b,c = 5,6,7
+
+
+# a,b,c = 5,6,7
+# print(a,b,c)
+
+
+#Keywords and Identifiers
+
+# to check keywords in python 
+
+import keyword
+print(keyword.kwlist)
