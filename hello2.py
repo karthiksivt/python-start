d24534b7849fd2fe6c3335049d294c340fcf8c71
karthik <karthiksivt@gmail.com>
2023-09-06 19:44:39 +0530

    escape sequences

--- hello2.py
+++ hello2.py
@@ -1,1 +1,13 @@
 print(''' Hello "This" is the 'test' ''')
+
+#escape sequences
+
+print("hello i/' am good")
+
+print("Line1\nLine2\nLine3")
+
+print("Hello \\ backslash")
+
+print("hello \t yup")
+
+print("Hello \" ")
