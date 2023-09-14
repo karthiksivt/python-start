40b438d110a6a6cf42f1ae93abe5d0c7bcab2a70
karthik <karthiksivt@gmail.com>
2023-09-14 23:00:38 +0530

    added comments

--- escape_seq.py
+++ escape_seq.py
@@ -35,3 +35,17 @@ print(f"hello ///.'\\\\\ ")
 #print emojies
 # this is the unicode of smiley U+1F600	, in the place of + replace 3 000 and at start backslash
 print("\U0001F600")
+
+# operators +, - , * , **(power) , / ( always give float o/p), // ( Floor divison)
+# Floor division always give the interger o/p
+
+# Always remember the BODMAS rule
+
+# () -> high Precedence
+# ** -> next 
+# +, - , * , all have equal but from left to right
+
+# only power is from right to left.
+
+# Rules to remember for naming the variable( it cant start with numbers)
+# no keywords , Can start with _ to name the variable
